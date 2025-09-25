fdb819727943a4cb19dd2dc372a64ca58ae040b9
saghaa06 <148466439+saghaa06@users.noreply.github.com>
2025-09-25 11:23:35 +0100

    Update app.py

--- app.py
+++ app.py
@@ -135,6 +135,7 @@ def history():
 # ---------- Lancement ----------
 if __name__ == "__main__":
     init_db()
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
+
 
